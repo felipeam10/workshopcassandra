a400b790cdedddd389e0aabfeb924bbf42432c5a
felipeam10 <felipeam10@hotmail.com>
2024-07-03 09:58:16 -0300

    vJSR-8.18 - PUT Department

--- src/main/java/com/philips/workshopcassandra/services/DepartmentService.java
+++ src/main/java/com/philips/workshopcassandra/services/DepartmentService.java
@@ -24,11 +24,9 @@ public class DepartmentService {
     }
 
     public DepartmentDTO findById(UUID id) {
-        Optional<Department> result = repository.findById(id);
-        Department entity = result.orElseThrow(() -> new ResourceNotFoundException("Id not found"));
+        Department entity = getById(id);
         return new DepartmentDTO(entity);
     }
-
     public DepartmentDTO insert(DepartmentDTO dto) {
         Department entity = new Department();
         entity.setId(UUID.randomUUID());
@@ -37,7 +35,19 @@ public class DepartmentService {
         return new DepartmentDTO(entity);
     }
 
+    public DepartmentDTO update(UUID id, DepartmentDTO dto) {
+        Department entity = getById(id);
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new DepartmentDTO(entity);
+    }
+
     private void copyDtoToEntity(DepartmentDTO dto, Department entity) {
         entity.setName(dto.getName());
+    }
+
+    private Department getById(UUID id) {
+        Optional<Department> result = repository.findById(id);
+        return result.orElseThrow(() -> new ResourceNotFoundException("Id not found"));
     }
 }
